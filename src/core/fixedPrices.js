c9a42e1a726f43d748a3aa81533e1ba2430d773b
thavananthan <it17068192@my.sliit.lk>
2020-08-14 01:33:08 +0530

    completed project"

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: '10000.00LKR to 30000.00LKR',
+    array: [10000, 20000],
+  },
+  {
+    _id: 2,
+    name: '31000.00LKR to 60000.00LKR',
+    array: [31000, 60000],
+  },
+  {
+    _id: 3,
+    name: '61000.00LKR to 100000.00LKR',
+    array: [61000, 100000],
+  },
+  {
+    _id: 4,
+    name: '110000.00LKR to 170000.00LKR',
+    array: [110000, 170000],
+  },
+  {
+    _id: 5,
+    name: 'More than 180000.00LKR',
+    array: [180000, 300000],
+  },
+];
